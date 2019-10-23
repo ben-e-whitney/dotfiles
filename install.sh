42a346579a64711258c10f65391707be6c65c4e6
Ben Whitney <whitneybe@ornl.gov>
2019-10-23 14:37:28 -0400

    ssh: Add initial configuration.

--- install.sh
+++ install.sh
@@ -2,7 +2,15 @@
 
 LN="ln --symbolic --interactive"
 
+${LN} "${PWD}/systemd" "${HOME}/.config/systemd"
 ${LN} "${PWD}/fish" "${HOME}/.config/fish"
 ${LN} "${PWD}/i3" "${HOME}/.config/i3"
 ${LN} "${PWD}/terminator" "${HOME}/.config/terminator"
 ${LN} "${PWD}/vim" "${HOME}/.vim"
+
+PAM_ENVIRONMENT="${HOME}/.pam_environment"
+
+#It looks like `XDG_RUNTIME_DIR` isn't set whenever this file gets read.
+#Providing a default.
+echo "XDG_RUNTIME_DIR DEFAULT=\"/run/user/$(id --user)\"" > "${PAM_ENVIRONMENT}"
+cat "${PWD}/PAM/pam_environment" >> "${PAM_ENVIRONMENT}"
