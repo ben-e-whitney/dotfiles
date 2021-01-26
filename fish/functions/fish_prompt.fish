8da7dceac6fd764720710378139225e200f7cbcd
Ben Whitney <whitneybe@ornl.gov>
2021-01-26 09:55:21 -0500

    fish: Add Git branch to prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,6 @@
 function fish_prompt
-	set_color yellow
-   printf '%s' (whoami)
-
-   set_color normal
-   printf ' in '
-
+   set BRANCHNAME (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+   and begin; set_color blue; printf "($BRANCHNAME) "; set_color normal; end
    set_color $fish_color_cwd
    printf '%s' (prompt_pwd)
    set_color normal
